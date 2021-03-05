370576c8616afbc4880eb3502457e8a5ad57365b
yanxin1 <yanxin1@staff.sina.com.cn>
2021-03-05 19:11:37 +0800

    :tada: 首次提交

---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { PageContainer } from '@ant-design/pro-layout';
+import { Card, Alert, Typography } from 'antd';
+import { useIntl, FormattedMessage } from 'umi';
+import styles from './Welcome.less';
+
+const CodePreview: React.FC = ({ children }) => (
+  <pre className={styles.pre}>
+    <code>
+      <Typography.Text copyable>{children}</Typography.Text>
+    </code>
+  </pre>
+);
+
+export default (): React.ReactNode => {
+  const intl = useIntl();
+  return (
+    <PageContainer>
+      <Card>
+        {intl.formatMessage({
+          id: 'menu.testlocale',
+          defaultMessage: 'testlocale',
+        })}
+      </Card>
+    </PageContainer>
+  );
+};
